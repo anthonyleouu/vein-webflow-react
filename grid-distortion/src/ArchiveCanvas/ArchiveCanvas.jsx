9ce954b9a74dfcb373d0617ab44256560f46976f
anthonyleouu <antoleounakis@gmail.com>
2026-03-18 20:37:48 +0200

    Increase parallax depth intensity

--- grid-distortion/src/ArchiveCanvas/ArchiveCanvas.jsx
+++ grid-distortion/src/ArchiveCanvas/ArchiveCanvas.jsx
@@ -168,7 +168,7 @@ const getParallaxOffset = (col) => {
 
           const parallax = getParallaxOffset(col);
           const screenX = col * cellW + s.x;
-          const screenY = row * cellH + s.y + masonryOffset + s.vy * parallax * 8;
+          const screenY = row * cellH + s.y + masonryOffset + s.vy * parallax * 16;
 
           if (img) {
             drawWarpedImage(img, screenX, screenY, blockW, blockH, s.speed);
