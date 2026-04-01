392920391198ea64b2bf7101715ad404d2c5ca9b
anthonyleouu <antoleounakis@gmail.com>
2026-04-01 21:31:40 +0300

    chore: reset to blank

--- grid-distortion/src/main.jsx
+++ grid-distortion/src/main.jsx
@@ -1,18 +1,1 @@
-import { createRoot } from 'react-dom/client';
-import WorkSlider from './WorkSlider/WorkSlider';
-
-window.mountAll = function mountAll() {
-  // Mount WorkSlider
-  const workContainer = document.getElementById('work-slider-root');
-  if (workContainer && !workContainer._mounted) {
-    workContainer._mounted = true;
-    const root = createRoot(workContainer);
-    root.render(<WorkSlider />);
-  }
-};
-
-if (document.readyState === 'loading') {
-  document.addEventListener('DOMContentLoaded', mountAll);
-} else {
-  mountAll();
-}
+"window.mountAll = function() {};"
